694a5d3b6e7a3314e9801b94221f523fb72acb26
coderxaron <schnitzelwolke@gmail.com>
2025-07-20 22:31:57 +0000

    version 1

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
+-- Module
 
+—[[
+Work in progress ._?
+]]
+
+local Framework = {}
+
+local loaded = false
+
+local function load()
+    loaded = true
+    return loaded
+end
+
+Framework.Loaded = loaded
+Framework.Load = load 
+
+return Framework
